06129272e9f0b191dbb50d654e51ab56eafef6d2
Hannan Razalli <a.hannanrazalli@gmail.com>
2025-12-29 17:00:19 +0800

    Pandas series

--- pandas1.py
+++ pandas1.py
@@ -2,5 +2,10 @@
 
 import pandas as pd
 
-df = pd.read_csv('xxx.csv') # Load data from a CSV file into a DataFrame
+#df = pd.read_csv('xxx.csv') # Load data from a CSV file into a DataFrame
 
+#print(pd.__version__) # Print the version of pandas library
+
+a = [1, 7, 2]
+myvar = pd.Series(a) # Create a pandas Series from a list
+print(myvar) # Print the Series
