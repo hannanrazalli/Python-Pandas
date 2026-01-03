b618eec9b5686308e56b0b17bf95697fd31521e8
Hannan <a.hannanrazalli@gmail.com>
2026-01-03 10:58:17 +0800

    df.iloc[1

--- pandas1.py
+++ pandas1.py
@@ -1,6 +1,25 @@
 import pandas as pd
 
-A = pd.Series(['a','b','c'], index = [1,3,5])
+marks_dict = ({
+  'A' : 85,
+  'B' : 75,
+  'C' : 65,
+  'D' : 55
+})
 
-print(A.loc[1:3])
-print(A.iloc[1:3])
+grade_dict = ({
+  'A' : 4,
+  'B' : 3.5,
+  'C' : 3,
+  'D' : 2.5
+})
+
+mark = pd.Series(marks_dict)
+grade = pd.Series(grade_dict)
+
+df = pd.DataFrame({
+    'Marks' : mark,
+    'Grades' : grade
+})
+
+print(df.iloc[1])
