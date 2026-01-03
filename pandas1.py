8f90f221e324eeda679e99b52a9a45a58085c5b5
Hannan <a.hannanrazalli@gmail.com>
2026-01-03 10:33:08 +0800

    loc & iloc

--- pandas1.py
+++ pandas1.py
@@ -1,27 +1,6 @@
 import pandas as pd
 
-grade_data = {
-    "A":4.0,
-    "B":3.5,
-    "C":3.0,
-    "D":2.5
-}
+A = pd.Series(['a','b','c'], index = [1,3,5])
 
-marks_dict = {
-    "A":85,
-    "B":75,
-    "C":65,
-    "D":55
-}
-
-grade = pd.Series(grade_data)
-marks = pd.Series(marks_dict)
-
-df = pd.DataFrame({
-    'Grade' : grade_data,
-    'Marks' : marks_dict
-})
-
-df['ScaledMarks'] = (df['Marks']/90*100).round(1)
-
-print(df)
+print(A.loc[1:3])
+print(A.iloc[1:3])
